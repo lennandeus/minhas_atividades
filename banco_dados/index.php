8b7173c488dc547dcd37ffe4705751d3984b43c2
JUAN VITOR TASCA DE ANDRADE <juan.4910@aluno.pr.senac.br>
2023-04-17 09:02:40 -0300

    banco de dados

---
+++ banco_dados/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>Menu Principal</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Buteco do Nunes</h1>
+        <div class="card" style="width: 18rem;">
+            <img src="img/xburguer.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">X-Nunes</h5>
+                <p class="card-text">Pão, Hambúrguer, Pão, Salmão, Carne de frango, Rato, Iguana, Picles, Tomate</p>
+                <a href="pedido.php" class="btn btn-primary">Faça seu pedido</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="img/pitu.png" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Água com gás</h5>
+                <p class="card-text">Garrafa de "água" 965ml</p>
+                <a href="pedido.php" class="btn btn-primary">Faça seu pedido</a>
+            </div>
+        </div>
+    </div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
+</html>
